d9c7a83c89a0e3919d17bcd5e00a9bbc120bca67
Berkay ÇIRAK <berkaycirak_11@hotmail.com>
2022-11-01 20:26:21 +0300

    Autoscroll is added

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,17 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ActiveUsers from './ActiveUsers';
 
 function Chat({ socket, username }) {
 	const [message, setMessage] = useState('');
 	const [messages, setMessages] = useState([]);
+	const [typingStatus, setTypingStatus] = useState();
 	const navigate = useNavigate();
+	const lastMessageRef = useRef(null);
 
 	const handleExit = () => {
 		navigate('/');
 		window.location.reload();
 	};
 
+	const handleTyping = () => {
+		if (message !== '') {
+			socket.emit('typing', `${username} is typing...`);
+		}
+	};
 	const handleSendMessage = (e) => {
 		e.preventDefault();
 
@@ -30,21 +37,32 @@ function Chat({ socket, username }) {
 		);
 	}, [socket, messages]);
 
+	// for notification
+	useEffect(() => {
+		socket.on('typingResponse', (data) => setTypingStatus(data));
+	}, [socket]);
+
+	// for auto-scroll
+	useEffect(() => {
+		lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
+	}, [messages]);
+
 	return (
 		<div className=' flex items-center justify-center h-[100vh] bg-red-300 p-12'>
 			<div>
 				<ActiveUsers socket={socket} />
 			</div>
 			<div className='flex flex-col min-w-[1100px] '>
-				<div className='self-end  mb-10'>
+				<div className='flex items-center gap-2 self-end  mb-10'>
+					<h1 className='font-bold'>{username}</h1>
 					<button
 						className='border p-2 rounded-md bg-black text-white transition hover:bg-red-500'
 						onClick={handleExit}>
 						Exit
 					</button>
 				</div>
-				<div className='border-2 w-[60%] min-h-[440px] self-center  bg-gray-100 overflow-y-scroll rounded-md '>
-					<div className=' text-2xl p-2'>
+				<div className='relative border-2 w-[60%] h-[440px] self-center  bg-gray-100 overflow-y-scroll rounded-md '>
+					<div className=' text-2xl p-2 '>
 						{messages.map((item) =>
 							item.name === username ? (
 								<div className='bg-red-200 border rounded-md m-2 p-4'>
@@ -59,6 +77,8 @@ function Chat({ socket, username }) {
 							)
 						)}
 					</div>
+					<div ref={lastMessageRef} />
+					<div className=' bottom-0'>{typingStatus}</div>
 				</div>
 				<div className='w-[60%] self-center'>
 					<textarea
@@ -68,6 +88,7 @@ function Chat({ socket, username }) {
 						placeholder='Write your message...'
 						value={message}
 						onChange={(e) => setMessage(e.target.value)}
+						onKeyUp={handleTyping}
 					/>
 					<button
 						className='border-2 rounded-md p-2'
--- server/index.js
+++ server/index.js
@@ -22,20 +22,19 @@ io.on('connection', (socket) => {
 	socket.on('disconnect', () => {
 		console.log('🔥: A user disconnected');
 		users = users.filter((user) => user.socketID !== socket.id);
-		// console.log(users);
-		//Sends the list of users to the client
+
 		io.emit('newUserResponse', users);
 	});
 
 	socket.on('message', (data) => {
 		io.emit('messageResponse', data);
 	});
-	//Listens when a new user joins the server
+	socket.on('typing', (data) =>
+		socket.broadcast.emit('typingResponse', data)
+	);
 	socket.on('newUser', (data) => {
-		//Adds the new user to the list of users
 		users.push(data);
-		// console.log(users);
-		//Sends the list of users to the client
+
 		io.emit('newUserResponse', users);
 	});
 });
